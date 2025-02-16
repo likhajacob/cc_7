4458b19909338a3422035333a0f6d9e67ba0e34b
Likha Jacob <likha.jacob03@gmail.com>
2025-02-16 17:13:08 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -16,3 +16,20 @@ function calculateHourlyWage(salary, hoursPerWeek) {
 
 console.log(`Hourly Wages: $${calculateHourlyWage(52000, 40)}`)//Output: Hourly Wages: $25.00
 console.log(`Hourly Wages: $${calculateHourlyWage(75000, 35)}`)//Output: Hourly Wage: $41.21
+// Task 3 Customer Loylty Discount 
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discount = 0;
+    if (years >= 5) {
+        discount = 0.15; // 15% discount will be applied
+    } else if (years >= 3) {
+        discount = 0.10; // 10% discount will be applied 
+    } else {
+        discount = 0.05; // 5% discount will be applied 
+    }
+    let discountedAmount = amount - (amount * discount);
+    console.log(`Discounted Amount: $${amount.toFixed(2)}`);
+};
+
+// Test Data 
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"

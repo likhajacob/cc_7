159c6f0e7da83c6b14d129634c2d85795ae8ff52
Likha Jacob <likha.jacob03@gmail.com>
2025-02-16 16:43:54 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -9,3 +9,10 @@ function calculateInvoice(subtotal, taxRate, discount){
 
 calculateInvoice(400, 0.05, 7); // Expected output $413
 calculateInvoice(750, 0.2, 4); // Expected output $896
+//Task 2 Function Expression
+function calculateHourlyWage(salary, hoursPerWeek) {
+    return (salary / (hoursPerWeek * 52)).toFixed(2)
+}// Function that calculates hourly wages
+
+console.log(`Hourly Wages: $${calculateHourlyWage(52000, 40)}`)//Output: Hourly Wages: $25.00
+console.log(`Hourly Wages: $${calculateHourlyWage(75000, 35)}`)//Output: Hourly Wage: $41.21
